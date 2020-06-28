ce5e31a2d94d5276d3306c954ab1d655079872a0
nx4n <konnnyaha1@gmail.com>
2020-06-28 20:53:59 +0400

    4

--- script.js
+++ script.js
@@ -32,11 +32,13 @@ appData.moneyPerDay = +(appData.theCash / 30).toFixed(2);
 alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
 if (appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка.');
+    alert('Минимальный уровень достатка.');
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-     console.log('Средний уровень достатка');
+    alert('Средний уровень достатка.');
 } else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка');
+    alert('Высокий уровень достатка.');
 } else {
-    console.log('Ошибка.');
+    alert('Ошибка.');
 }
+
+console.log(appData);
